4bc821c19244ee0f6ed63ddb8fe23d2f297c5bf9
Brian Wangora <brianwangora@gmail.com>
2022-06-04 18:44:22 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+let text = document.createTextNode("Brian is the champion");
+newHeader.appendChild(text);
